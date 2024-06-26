f6f862527cd103b3e1738fba89631632ab937f2d
duongnh <nguyenhaid98@yahoo,cin>
2024-06-26 18:55:49 +0700

    add route

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,7 +2,10 @@
 const router = require("express").Router();
 // const userRoute = require('./user')
 const authRoute = require('./authRoute')
+
+const bookingRoute = require('./bookingRoute')
 //  router.use('/auth', userRoute);
 router.use('/user', authRoute)
+router.use('/booking', bookingRoute)
 
  module.exports = router
